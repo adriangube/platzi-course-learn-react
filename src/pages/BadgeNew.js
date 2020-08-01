a90019f0ab908ef107fabc55eaee2688bfa2ae6e
Adrian Guillen <adrianguillenbermudez@gmail.com>
2020-08-01 19:02:02 +0200

    chore: share state between BadgeForm.js and Badge.js

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -2,10 +2,29 @@ import React from "react";
 
 import Navbar from "../components/Navbar";
 import Badge from "../components/Badge";
+import BadgeForm from "../components/BadgeForm";
 import header from "../images/badge-header.svg";
 import "./styles/BadgeNew.css";
 
 class BadgeNew extends  React.Component {
+    state = {
+        form: {
+            firstName: '',
+            lastName: '',
+            email: '',
+            jobTitle: '',
+            twitter: ''
+        }
+    }
+
+    handleChange = e => {
+        this.setState({
+            form: {
+                ...this.state.form,
+                [e.target.name]: e.target.value
+            }
+        })
+    }
     render(){
         return (
             <div>
@@ -15,14 +34,17 @@ class BadgeNew extends  React.Component {
                 </div>
                 <div className="container">
                     <div className="row">
-                        <div className="col">
+                        <div className="col-6">
                             <Badge
-                                firstName="Adrián"
-                                lastName="Guillén"
-                                twitter="adriangube"
-                                jobTitle="Frontend Engineer"
+                                firstName={this.state.form.firstName}
+                                lastName={this.state.form.lastName}
+                                twitter={this.state.form.twitter}
+                                jobTitle={this.state.form.jobTitle}
                                 avatarUrl="https://avatars1.githubusercontent.com/u/24369034?s=460&u=a90581d13ceae3f118d917b6f2ead5f44d3a2cdd&v=4"
                             />
+                        </div>
+                        <div className="col-6">
+                            <BadgeForm onChange={this.handleChange} formValues={this.state.form}/>
                         </div>
                     </div>
                 </div>
