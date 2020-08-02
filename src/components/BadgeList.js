c2fd9194785f477467023b69d589f9224e4252e5
Adrian Guillen <adrianguillenbermudez@gmail.com>
2020-08-02 17:27:17 +0200

    chore: fetch from local api and handle loading and error petitions

--- src/components/BadgeList.js
+++ src/components/BadgeList.js
@@ -1,9 +1,20 @@
 import React from "react";
 import BadgesListItem from "./BadgesListItem";
 import './styles/BadgesList.css';
+import {Link} from "react-router-dom";
 
 class BadgesList extends React.Component {
     render() {
+
+        if(this.props.badges.length === 0){
+            return (
+                <div>
+                    <h3>No badges where found</h3>
+                    <Link className="btn btn-primary" to="/badges/new">Create new badge</Link>
+                </div>
+            )
+        }
+
         return (
             <div className="BadgesList">
                 <ul className="list-unstyled">
